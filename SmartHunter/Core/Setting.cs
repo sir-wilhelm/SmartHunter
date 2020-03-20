ad2c7f71326583e3fa218d7ee2eb33c4c3ef475d
sir-wilhelm <willstrohmeier@gmail.com>
2020-03-20 12:56:34 -0500

    Added settings tab

---
+++ SmartHunter/Core/Setting.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace SmartHunter.Core
+{
+    public class Setting
+    {
+        public bool Value { get; set; }
+        public string Name { get; }
+        public string Description { get; }
+        public List<Setting>SubSettings { get; }
+        public Command TriggerAction { get; }
+        public Setting(bool value, string name, string description, Command action = null)
+        {
+            Value = value;
+            Name = name;
+            Description = description;
+            SubSettings = new List<Setting>();
+            TriggerAction = action;
+        }
+    }
+}
